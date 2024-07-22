d6603e10fa3b2e241df9fc050b38517f212bd8e0
Kirill <kiriltimoxov@gmail.com>
2024-07-22 18:00:29 +0500

    +Рефакторинг для js на List +Исправлен формат даты, передаваемый через SignalR +Исправлен баг, связанный с не возможностью выбора первой команды в выподающем списке на странице Add +Добавлено отображение в режиме реального времени через вклядку edit (Изменение и удаление)

--- Footballers v2/Hubs/FootballersHub.cs
+++ Footballers v2/Hubs/FootballersHub.cs
@@ -8,8 +8,20 @@ namespace Footballers_v2.Hubs
     {
         public async Task Send(Footballer model)
         {
+            //model.Birthday = 
             model.Country = ((Country)int.Parse(model.Country)).ToString();
             await this.Clients.All.SendAsync("Receive", model);
+        }
+
+        public async Task Editor(Footballer model)
+        {
+            model.Country = ((Country)int.Parse(model.Country)).ToString();
+            await this.Clients.All.SendAsync("ReceiveEdit", model);
+        }
+
+        public async Task Deleter(string id)
+        {
+            await this.Clients.All.SendAsync("ReceiveDelete", id);
         }
     }
 }
