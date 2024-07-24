baecf6da15d374f3a724e35a5db4182fbc5423a7
Kirill <kiriltimoxov@gmail.com>
2024-07-24 13:24:09 +0500

    +На странице рдактирования исправлено отображение Страны +Исправлен баг, привозвращении из редактирования на лист при помощи кнопки назад, проподали данные переданные через SignalR

--- Footballers v2/Hubs/FootballersHub.cs
+++ Footballers v2/Hubs/FootballersHub.cs
@@ -6,19 +6,21 @@ namespace Footballers_v2.Hubs
 {
     public class FootballersHub: Hub
     {
-        public async Task Send(Footballer model)
+        //Отправка
+        public async Task Sender(Footballer model)
         {
-            //model.Birthday = 
             model.Country = ((Country)int.Parse(model.Country)).ToString();
             await this.Clients.All.SendAsync("Receive", model);
         }
 
+        //Изменение
         public async Task Editor(Footballer model)
         {
             model.Country = ((Country)int.Parse(model.Country)).ToString();
             await this.Clients.All.SendAsync("ReceiveEdit", model);
         }
 
+        //Удаление
         public async Task Deleter(string id)
         {
             await this.Clients.All.SendAsync("ReceiveDelete", id);
